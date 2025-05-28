9fc12663628816895e08f7e23ef578e7ea4ab3e7
ILIAN ILIEV <iiliev84@gmail.com>
2025-05-27 21:32:51 -0500

    schema and files set up

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 import express from "express";
 const app = express();
 export default app;
+import filesRouter from "#api/files";
+import foldersRouter from "#api/folders";
+
+app.use(express.json())
+
+app.use("/files", filesRouter);
+app.use("/folders", foldersRouter);
+
+
+app.use((err,req,res,next)=>{
+    console.log(err)
+    res.status(500).send("An error occured " + err)
+})
